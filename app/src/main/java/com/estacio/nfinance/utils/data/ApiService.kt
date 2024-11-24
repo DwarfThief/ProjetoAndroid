39feb673a734cdffd7b4883a5e18179b4e0acf35
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2024-11-24 16:49:30 -0300

    [fix] 🧠 headers

--- app/src/main/java/com/estacio/nfinance/utils/data/ApiService.kt
+++ app/src/main/java/com/estacio/nfinance/utils/data/ApiService.kt
@@ -5,9 +5,11 @@ import com.estacio.nfinance.models.api.RegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
+    @Headers("Content-Type: application/json")
     @POST("login")
     fun postLogin(@Body request: LoginRequest): Call<LoginResponse>
 
