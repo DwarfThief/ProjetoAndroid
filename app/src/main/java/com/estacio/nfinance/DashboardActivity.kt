86f810e67ed337aef2878896c25d27c5d6ccb556
Brenno 'DwarfThief' Barboza <brennol.c.b@gmail.com>
2024-11-25 20:07:04 -0300

    [Feat] Adicionando GET no Dashboard

--- app/src/main/java/com/estacio/nfinance/DashboardActivity.kt
+++ app/src/main/java/com/estacio/nfinance/DashboardActivity.kt
@@ -1,12 +1,18 @@
 package com.estacio.nfinance
 
+import ApiService
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.estacio.nfinance.models.api.ListTransactionResponse
+import com.estacio.nfinance.models.api.RegisterResponse
+import retrofit2.Call
+import retrofit2.Response
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,5 +44,31 @@ class DashboardActivity : AppCompatActivity() {
             val intent = Intent(this, CreateActivity::class.java)
             startActivity(intent) // Inicia a nova Activity
         }
+
+        // TODO: Criar função para pegar o Id do user
+        val userId = "66909ad31211260fe9c2a6bd"
+        val typeTransaction = 0
+
+        val retrofitClient = RetrofitService.getRetrofitInstance("http://localhost:3001/")
+        val endpoint = retrofitClient.create(ApiService::class.java)
+
+        endpoint.getTransactionsByType(userId,typeTransaction).enqueue(object : retrofit2.Callback<ListTransactionResponse> {
+            override fun onResponse(
+                call: Call<ListTransactionResponse>,
+                response: Response<ListTransactionResponse>
+            ) {
+                if (response.isSuccessful) {
+                    // TODO: Alimentar o recyclerview
+                } else {
+                    Toast.makeText(this@DashboardActivity, "Falha na requisição: ${response.body().toString()}",
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ListTransactionResponse>, t: Throwable) {
+                Toast.makeText(this@DashboardActivity, "Falha na requisição: ${t.message}",
+                    Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
